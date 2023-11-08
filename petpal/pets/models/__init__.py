df0cc39c908f5b34bc16f74475d92429aa816d96
oujiayi <kelly.ou@mail.utoronto.ca>
2023-11-07 21:34:21 -0500

    created a folder for models.

---
+++ petpal/pets/models/__init__.py
@@ -0,0 +1,4 @@
+from .pet import Pet
+from .application import Application
+from .comment import ShelterComment, ApplicationComment
+from .notification import Notification
---
+++ petpal/pets/models/comment.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class ShelterComment(models.Model):
+    pass
+
+class ApplicationComment(models.Model):
+    pass
