20931b68b9379038f64af765d233b0bce8894462
oujiayi <kelly.ou@mail.utoronto.ca>
2023-11-07 21:26:30 -0500

    created empty models

--- petpal/pets/models.py
+++ petpal/pets/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Pet(models.Model):
+    pass
+
+class Application(models.Model):
+    pass
+
+class ShelterComment(models.Model):
+    pass
+
+class ApplicationComment(models.Model):
+    pass
+
+class Notification(models.Model):
+    pass
