19ee9fd389b5ebdfc269639419c695a12bdd0147
oujiayi <kelly.ou@mail.utoronto.ca>
2023-11-07 23:12:15 -0500

    finished comments and notifications models.

--- petpal/pets/models/comment.py
+++ petpal/pets/models/comment.py
@@ -1,7 +1,19 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator 
+from accounts.models import ShelterUser, CustomUser, PetUser
+from application import Application
 
 class ShelterComment(models.Model):
-    pass
+    shelter = models.ForeignKey(ShelterUser, on_delete=models.CASCADE)
+    user_from = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    message = models.TextField()
+    rating = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    time_created = models.DateTimeField(auto_now=True)
+    
 
 class ApplicationComment(models.Model):
-    pass
+    message = models.TextField()
+    user_from = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    user_to = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    application = models.ForeignKey(Application, on_delete=models.CASCADE)
+    time_created = models.DateTimeField(auto_now=True)
--- petpal/pets/models/notification.py
+++ petpal/pets/models/notification.py
@@ -1,4 +1,9 @@
 from django.db import models
+from accounts.models import CustomUser
 
 class Notification(models.Model):
-    pass
+    read = models.BooleanField(default=False)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    message = models.TextField()
+    link = models.URLField()
+    time_created = models.DateTimeField(auto_now=True)
