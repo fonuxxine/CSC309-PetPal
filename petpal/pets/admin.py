34849c2667e1b0b78bba2b57491628a97bd322cb
oujiayi <kelly.ou@mail.utoronto.ca>
2023-11-08 21:37:34 -0500

    registered all models to admin

--- petpal/pets/admin.py
+++ petpal/pets/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Pet)
+admin.site.register(Applications)
+admin.site.register(ShelterComment)
+admin.site.register(ApplicationComment)
+admin.site.register(Notification)
