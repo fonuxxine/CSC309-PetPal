08227c6eac4536d95f3ec6a3393d9edab53e9c32
Nicknames43 <nicholas.pedro@mail.utoronto.ca>
2023-11-11 16:08:17 -0500

    updated sort, filters and pagination

--- petpal/pets/views/pets_views.py
+++ petpal/pets/views/pets_views.py
@@ -1,72 +1,144 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView, RetrieveAPIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView,
+    ListAPIView,
+    RetrieveAPIView,
+)
 from pets.models import *
 from ..serializers.pet_serializers import *
 from accounts.models import ShelterUser
+from rest_framework import permissions
+from django.db.models import Q
+from rest_framework.pagination import LimitOffsetPagination
 
-class ShelterPetsList(ListAPIView):
+
+class IsShelterLoggedIn(permissions.BasePermission):
+    def has_permission(self, request, view):
+        shelter_user = get_object_or_404(ShelterUser, id=view.kwargs["shelter_id"])
+        if request.user.id == shelter_user.id:
+            return True
+        return False
+
+
+class IsPetInShelter(permissions.BasePermission):
+    def has_permission(self, request, view):
+        pet = get_object_or_404(Pet, id=view.kwargs["pet_id"])
+        if request.user.id == pet.shelter.id:
+            return True
+        return False
+
+
+class ShelterPetsListCreate(ListCreateAPIView):
     serializer_class = PetSerializer
-    permission_classes = [IsAuthenticated]
-    
-    def get_queryset(self):
-        shelter = get_object_or_404(ShelterUser, id=self.kwargs['shelter_id'])
-        queryset = Pet.objects.all().filter(shelter=shelter)
-        status = self.request.query_params.get("status")
-        type = self.request.query_params.get("type")
-        breed = self.request.query_params.get("breed")
-        age = self.request.query_params.get("age")
-
-        if type is not None:
-            queryset = queryset.filter(type=type)
-        if breed is not None:
-            queryset = queryset.filter(breed=breed)
-        if status is not None:
-            queryset = queryset.filter(status=status)
-        if age is not None:
-            queryset = queryset.filter(age=age)
-        return queryset
-
-class ShelterPetsCreate(CreateAPIView):
-    serializer_class = PetSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, IsShelterLoggedIn]
+    pagination_class = LimitOffsetPagination
 
     def perform_create(self, serializer):
-        shelter = get_object_or_404(ShelterUser, id=self.kwargs['shelter_id'])
+        shelter = get_object_or_404(ShelterUser, id=self.kwargs["shelter_id"])
         serializer.save(shelter=shelter)
+
+    def get_queryset(self):
+        shelter = get_object_or_404(ShelterUser, id=self.kwargs["shelter_id"])
+        queryset = Pet.objects.all().filter(shelter=shelter)
+        params = self.request.GET
+
+        query = params.get("search")
+        if query:
+            queryset = queryset.filter(
+                Q(name__icontains=query)
+                | Q(description__icontains=query)
+                | Q(medical_history__icontains=query)
+                | Q(special_requirements__icontains=query)
+                | Q(behaviour__icontains=query)
+            )
+
+        query = params.getlist("status")
+        if query != []:
+            queryset = queryset.filter(status__in=query)
+        else:
+            queryset = queryset.filter(status="AV")
+
+        query = params.getlist("type")
+        if query != []:
+            queryset = queryset.filter(status__in=query)
+
+        query = params.getlist("breed")
+        if query != []:
+            queryset = queryset.filter(status__in=query)
+
+        query = params.getlist("age")
+        if query != []:
+            queryset = queryset.filter(status__in=query)
+
+        sort = params.get("sort", "name")
+        desc = params.get("desc")
+        if desc:
+            sort = "-" + sort
+
+        return queryset.order_by(sort)
+
 
 class ShelterPetsRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
     serializer_class = PetSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [
+        permissions.IsAuthenticated,
+        IsShelterLoggedIn,
+        IsPetInShelter,
+    ]
 
     def get_object(self):
-        return get_object_or_404(Pet, id=self.kwargs['pet_id'])
+        return get_object_or_404(Pet, id=self.kwargs["pet_id"])
+
 
 class UserPetsList(ListAPIView):
     serializer_class = PetSerializer
+    pagination_class = LimitOffsetPagination
 
     def get_queryset(self):
         queryset = Pet.objects.all()
-        status = self.request.query_params.get("status")
-        shelter = self.request.query_params.get("shelter")
-        type = self.request.query_params.get("type")
-        breed = self.request.query_params.get("breed")
-        age = self.request.query_params.get("age")
-        sort = self.request.query_params.get("sort")
+        params = self.request.GET
 
-        if sort == '' or sort is None:
+        query = params.get("search")
+        if query:
+            queryset = queryset.filter(
+                Q(name__icontains=query)
+                | Q(description__icontains=query)
+                | Q(medical_history__icontains=query)
+                | Q(special_requirements__icontains=query)
+                | Q(behaviour__icontains=query)
+            )
+
+        query = params.getlist("status")
+        if query != []:
+            queryset = queryset.filter(status__in=query)
+        else:
+            queryset = queryset.filter(status="AV")
+
+        query = params.getlist("shelter")
+        if query != []:
+            queryset = queryset.filter(status__in=query)
+
+        query = params.getlist("type")
+        if query != []:
+            queryset = queryset.filter(status__in=query)
+
+        query = params.getlist("breed")
+        if query != []:
+            queryset = queryset.filter(status__in=query)
+
+        query = params.getlist("age")
+        if query != []:
+            queryset = queryset.filter(status__in=query)
+
+        sort = params.get("sort")
+        desc = params.get("desc")
+        if sort and desc:
+            sort = "-" + sort
+        elif desc:
+            sort = "-name"
+        elif not sort:
             sort = "name"
-        
-        if shelter is not None:
-            queryset = queryset.filter(shelter=shelter)
-        if status is not None:
-            queryset = queryset.filter(status=status)
-        if type is not None:
-            queryset = queryset.filter(type=type)
-        if breed is not None:
-            queryset = queryset.filter(breed=breed)
-        if age is not None:
-            queryset = queryset.filter(age=age)
         return queryset.order_by(sort)
 
 
@@ -74,4 +146,4 @@ class UserPetsRetrieve(RetrieveAPIView):
     serializer_class = PetSerializer
 
     def get_object(self):
-        return get_object_or_404(Pet, id=self.kwargs['pet_id'])
+        return get_object_or_404(Pet, id=self.kwargs["pet_id"])
