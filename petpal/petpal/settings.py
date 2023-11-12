dda34affc334c431ed8df91c136673fe430353a1
oujiayi <kelly.ou@mail.utoronto.ca>
2023-11-12 13:32:50 -0500

    Changed access token time to 120 mins

--- petpal/petpal/settings.py
+++ petpal/petpal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -147,3 +148,6 @@ REST_FRAMEWORK = {
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "media/"
 
+SIMPLE_JWT = { 
+     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=120)
+}
