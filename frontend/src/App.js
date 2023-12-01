8d437033192db62a687e1e3cda457f9f3aee54db
oujiayi <kelly.ou@mail.utoronto.ca>
2023-12-01 14:22:41 -0500

    change navbar when logged in.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,15 @@ import NavBar from "./components/NavBar";
 import AuthNavBar from "./components/AuthNavBar";
 import Landing from "./pages/landing";
 import Login from "./pages/login";
+import ManagePets from "./pages/manage";
 function App() {
   return (
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<NavBar/>}>
+          <Route path="/" element={localStorage.getItem('access_token') === null? <NavBar/> : <AuthNavBar/>}>
             <Route index element={<Landing/>} />
             <Route path="login/" element={<Login />} />
+            <Route path="/shelter/:id/manage/" element={<ManagePets/>}/>
           </Route>
         </Routes>
       </BrowserRouter>
