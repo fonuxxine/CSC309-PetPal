2228671c72f519cf5b5b2a1ec880207e145bef6e
alyson647 <alyson.roh@gmail.com>
2023-12-07 00:07:29 -0500

    list shelter page pagination

--- frontend/src/pages/allShelters/index.jsx
+++ frontend/src/pages/allShelters/index.jsx
@@ -3,12 +3,17 @@ import { useState, useEffect } from "react";
 import "./style.css";
 import { Route, Link, useParams } from "react-router-dom";
 
-const shelterURL = "/accounts/shelter/";
+
 
 function AllShelters() {
 
 
     const [shelters, setShelters] = useState([]);
+    const [shelterPage, setShelterPage] = useState(1);
+    const [shelterNext, setShelterNext] = useState(true);
+    const [shelterPrev, setShelterPrev] = useState(false);
+
+    const shelterURL = `/accounts/shelter/?page=${shelterPage}`;
 
     useEffect(() => {
         async function fetchShelters() {
@@ -17,11 +22,13 @@ function AllShelters() {
             })
             .then((response) => response.json())
             .then((json) => {
+                setShelterNext(json.next !== null);
+                setShelterPrev(json.previous !== null);
                 setShelters(json.results);
             });
         }
         fetchShelters();
-    }, [])
+    }, [shelterPage]);
 
     return (
         <div>
@@ -45,6 +52,44 @@ function AllShelters() {
                     </div>
                 </div>
             ))}
+            {shelterNext ? (
+          shelterPrev ? (
+            <div className="d-flex justify-content-between my-2">
+              <button
+                className="btn btn-primary btn-outline-dark btn-review"
+                onClick={() => setShelterPage(shelterPage - 1)}
+              >
+                Prev
+              </button>
+              <button
+                className="btn btn-primary btn-outline-dark btn-review"
+                onClick={() => setShelterPage(shelterPage + 1)}
+              >
+                Next
+              </button>
+            </div>
+          ) : (
+            <div className="d-flex justify-content-end my-2">
+              <button
+                className="btn btn-primary btn-outline-dark btn-review"
+                onClick={() => setShelterPage(shelterPage + 1)}
+              >
+                Next
+              </button>
+            </div>
+          )
+        ) : shelterPrev ? (
+          <div className="d-flex justify-content-start my-2">
+            <button
+              className="btn btn-primary btn-outline-dark btn-review"
+              onClick={() => setShelterPage(shelterPage - 1)}
+            >
+              Prev
+            </button>
+          </div>
+        ) : (
+          <></>
+        )}
         </div>
     )
 }
