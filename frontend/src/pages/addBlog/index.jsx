98430054be4e06dfcdb1f045c5866839d748f343
alyson647 <alyson.roh@gmail.com>
2023-12-08 07:27:57 -0500

    fixed blog image upload bug

--- frontend/src/pages/addBlog/index.jsx
+++ frontend/src/pages/addBlog/index.jsx
@@ -1,14 +1,11 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
 
 let bearer = "Bearer " + localStorage.getItem("access_token");
 
 function AddBlog() {
 
-    const [ title, setTitle ] = useState("");
-    const [ image, setImage ] = useState(null);
-    const [ content, setContent ] = useState("");
+    const [ blogInfo, setBlogInfo] = useState({});
     const [ errors, setErrors] = useState("");
 
     const { shelterID } = useParams();
@@ -20,16 +17,19 @@ function AddBlog() {
     }
 
     function addBlog() {
+        const formData = new FormData();
+        for (let key in blogInfo) {
+            if (blogInfo[key] !== null) {
+              formData.append(key, blogInfo[key]);
+            }
+        }
+
         fetch(`/blog/${shelterID}/`, {
             method: "POST",
             headers: {
                 Authorization: bearer,
             },
-            body: JSON.stringify({
-                title: title,
-                photo: image,
-                content: content,
-            }),
+            body: formData,
         })
         .then((response) => {
             return response.json();
@@ -43,6 +43,26 @@ function AddBlog() {
             }
         });
     }
+
+    // function handleFileChange(event) {
+    //     const selectedFile = event.target.files[0];
+    //     const allowedTypes = ["image/jpeg", "image/png", "image/jpg"];
+    
+    //     if (!allowedTypes.includes(selectedFile?.type)) {
+    //       setErrors((errors) => ({
+    //         ...errors,
+    //         profile_pic: "Must be JPG, JPEG or PNG",
+    //       }));
+    //     } else {
+    //         setBlogInfo({ ...blogInfo, photo: selectedFile });
+    //         setErrors((errors) => ({ ...errors, profile_pic: "" }));
+    //     //   const file = new FileReader();
+    //     //   file.onload = function () {
+    //     //     setProfilePicPreview(file.result);
+    //     //   };
+    //     //   file.readAsDataURL(selectedFile);
+    //     }
+    //   }
 
 
     return (
@@ -60,33 +80,40 @@ function AddBlog() {
             <div className="d-flex justify-content-center pt-4">
                 <div className="w-75">
                     <div className="form-group p-2">
-                    {errors === "" ? (<p className="login-error p-0">{errors}</p>) : (<></>)}
+                    {errors ? (<p className="login-error p-0">{errors}</p>) : (<></>)}
                         <label>Title</label>
                         <input 
                             type="text"
                             className="form-control"
                             placeholder="Enter your title"
-                            onChange={(event) => setTitle(event.target.value)}
+                            onChange={(event) =>
+                                setBlogInfo({ ...blogInfo, title: event.target.value })
+                              }
+                            // onChange={(event) => setTitle(event.target.value)}
                             required
                         />
                     </div>
                     <div className="form-group p-2">
                         <label>Photo</label>
+                        {/* <label
+            htmlFor="profile"
+            className="btn btn-primary position-absolute bottom-0 start-0"
+          ></label> */}
                         <input 
                             type="file"
                             className="form-control"
-                            onChange={(event) => setImage(event.target.files[0])}
+                            onChange={(event) =>
+                                setBlogInfo({ ...blogInfo, photo: event.target.files[0] })
+                              }
                             required
                         />
                     </div>
                     <div className="form-group p-2">
                         <label>Content</label>
-                        <input 
-                            type="textarea"
-                            className="form-control"
-                            onChange={(event) => setContent(event.target.value)}
-                            required
-                        />
+                        <textarea className="form-control" onChange={(event) =>
+                                setBlogInfo({ ...blogInfo, content: event.target.value })
+                              }
+                            required/>
                     </div>
                     <div><button className="login-but mt-2 p-2" onClick={() => addBlog()}>Submit</button></div>
                 </div>
